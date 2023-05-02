a86f212fb29a537057a8ec949f5bf1045e26d1c4
MarcCives <104699165+MarcCives@users.noreply.github.com>
2023-05-02 18:11:47 +0200

    Actualización código
    Tener un diccionario con las palabras incorrectas junto a sus posiciones en la imagen, width y height

--- algorithms/OCR/main.py
+++ algorithms/OCR/main.py
@@ -4,62 +4,76 @@ from PIL import Image
 url = 'https://languagetool.org/api/v2/check'
 headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+
 # Carga la imagen
-img = Image.open('test4.jpeg')
+img = Image.open(r'C:\Users\cives\OneDrive\Escritorio\test1.jpeg')
 
 # Convierte la imagen a escala de grises
 img = img.convert('L')
 
 # Reconoce el texto en la imagen
 text = pytesseract.image_to_string(img)
+
 # Imprime el texto reconocido
 print(text)
 
 # Reconoce el texto y la posición de cada palabra en la imagen
 data = pytesseract.image_to_data(img, output_type=pytesseract.Output.DICT)
 
-# Itera sobre cada palabra reconocida en la imagen
-for i in range(len(data['text'])):
+# Inicializar un diccionario vacío para almacenar los datos
+palabras = {}
+
+# Iterar a través de los datos y agregar cada palabra al diccionario
+long = len(data['text'])
+for i in range(long):
     # Obtén la palabra y su posición en la imagen
-    word = data['text'][i]
+    word = data['text'][i].replace(" ", "")  # eliminar los espacios en blanco  
+    word = word.replace(",", "")
     x = data['left'][i]
     y = data['top'][i]
     w = data['width'][i]
     h = data['height'][i]
     
-    # Imprime la palabra y su posición en la imagen
-    print('Palabra: {} | Posición: ({}, {}) - ({}, {})'.format(word, x, y, x+w, y+h))
+    w = x + w
+    h = y + h
     
+    # Agregar la palabra y sus datos de posición al diccionario
+    palabras[word] = [x, y, w, h]   
 
-
+# Detección de errores para las palabras
 data = {'text': text, 'language': 'ca', 'enabledOnly': 'false'}
 
 response = requests.post(url, headers=headers, data=data)
 
-errors = []
-
 matches = response.json().get('matches', [])
+errors = []
 
 for match in matches:
     if match['rule']['issueType'] == 'misspelling':
         errors.append((match['offset'], match['offset']+match['length'], match['message']))
 
-palabras = [];
+palabras_error = [];
+print(errors[::-1])
 # Indicar las palabras con error
 for error in errors[::-1]:
     start = text[:error[0]].rfind(' ') + 1
     end = text[error[1]:].find(' ') + error[1]
     if end == -1:
         end = len(text)
-    word = text[start:end]
-   # if word in data['text']:
-       # palabras['text'].append(data['text'][data['text'].index(word)]);
-        #palabras['left'].append(data['left'][data['text'].index(word)]);
-        #palabras['top'].append(data['top'][data['text'].index(word)]);
-        #palabras['width'].append(data['width'][data['text'].index(word)]);
-        #palabras['height'].append(data['height'][data['text'].index(word)]);
-    print(word)
+    word = text[start:end + 1]
+    word = word.replace(" ", "")
+    word = word.replace(",", "")
+    palabras_error.append(word)
+    print(word)    
 
+print(palabras_error)
 
-print(errors)
+#Guardamos un diccionario unicamente con los datos de las palabras incorrectas
+palabras_final = {}
+for palabra in palabras_error:
+    if palabra in palabras:
+        #Guardamos la palabra con el error y la posicion
+        palabras_final[palabra] = palabras[palabra]
 
+print(palabras_final)
+        
