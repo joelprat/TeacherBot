7eeb76911da5cb238ab3936d3ae1c20814a9c541
Joel Prat Vilanova <99801191+joelprat@users.noreply.github.com>
2023-06-02 16:04:01 +0200

    install_dependencies_windows.bat

---
+++ algorithms/OCR/install_dependencies_windows.bat
@@ -0,0 +1,36 @@
+@echo off
+
+REM Instalar pytesseract
+pip install pytesseract
+
+REM Instalar easyocr
+pip install easyocr
+
+REM Instalar opencv-python
+pip install opencv-python
+
+REM Instalar requests
+pip install requests
+
+REM Instalar translate
+pip install translate
+
+REM Instalar gtts
+pip install gtts
+
+REM Instalar Pillow
+pip install Pillow
+
+REM Instalar faker
+pip install faker
+
+REM Instalar SpeechRecognition
+pip install SpeechRecognition
+
+REM Instalar pydub
+pip install pydub
+
+REM Instalar pygame
+pip install pygame
+
+echo ¡Todas las dependencias han sido instaladas correctamente!
