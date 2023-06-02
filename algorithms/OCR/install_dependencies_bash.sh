516cfc0a83b91defd872feaf0c6ff7167abff29f
Joel Prat Vilanova <99801191+joelprat@users.noreply.github.com>
2023-06-02 16:04:41 +0200

    install_dependencies_bash.sh

---
+++ algorithms/OCR/install_dependencies_bash.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Instalar pytesseract
+pip install pytesseract
+
+# Instalar easyocr
+pip install easyocr
+
+# Instalar opencv-python
+pip install opencv-python
+
+# Instalar requests
+pip install requests
+
+# Instalar translate
+pip install translate
+
+# Instalar gtts
+pip install gtts
+
+# Instalar Pillow
+pip install Pillow
+
+# Instalar faker
+pip install faker
+
+# Instalar SpeechRecognition
+pip install SpeechRecognition
+
+# Instalar pydub
+pip install pydub
+
+# Instalar pygame
+pip install pygame
+
+
+echo "¡Todas las dependencias han sido instaladas correctamente!"
